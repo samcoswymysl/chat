4a02c4bd90343718947028bf3e399b7ad5c7c79b
samcoswymysl <rafalkrukowskipraca123@gmail.com>
2021-10-24 16:52:49 +0200

    Add information about the current list of chat participants

--- app.js
+++ app.js
@@ -4,27 +4,55 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+const activeUsers = [];
+const activeUsersNick = [];
+
+const refactorActive = (action, userName) => {
+  if (action === 'disconnect') {
+    const indexUser = activeUsers.findIndex((el) => el.id === userName.id);
+    activeUsers.splice(indexUser, 1);
+    activeUsersNick.length = 0;
+    activeUsers.map((user) => activeUsersNick.push(user.userName));
+  } else if (action === 'connect') {
+    activeUsers.push(userName);
+    activeUsersNick.push(userName.userName);
+  }
+};
+
 server.listen(3000, 'localhost', () => {
   console.log('port 3000');
 });
 
 app.use(express.static('public'));
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   socket.on('message', (data) => {
-    console.log(socket.userName, data);
     socket.broadcast.emit('message', {
       user: socket.userName,
       message: data,
     });
   });
 
-  socket.on('Login', (userName) => {
-    socket.userName = userName;
-    console.log(socket.userName);
+  socket.on('Login', (user) => {
+    socket.userName = user.userName;
+    refactorActive('connect', user);
 
+    socket.emit('Login', {
+      activeUsersNick,
+    });
     socket.broadcast.emit('Login', {
-      user: socket.userName,
+      activeUsersNick,
+    });
+
+    socket.on('disconnect', async () => {
+      const userName = {
+        userName: socket.userName,
+        id: socket.id,
+      };
+      await refactorActive('disconnect', userName);
+      socket.broadcast.emit('disconnectUser', {
+        activeUsersNick,
+      });
     });
   });
 });
--- public/front.js
+++ public/front.js
@@ -2,32 +2,62 @@ const socket = io();
 const messageInput = document.querySelector('.message');
 const userNameInput = document.querySelector('.userName');
 const usersList = document.querySelector('.userList');
-const divs = document.querySelectorAll('div');
+const divs = document.querySelectorAll('.divToggle');
 
-const ul = document.querySelector('ul');
-let yourName;
+const messageWindow = document.querySelector('.messageWindow');
+let userName;
 
 const creteMessage = (message, user) => {
-  const newMessage = document.createElement('li');
-  newMessage.innerText = `${user}: ${message}`;
-  ul.append(newMessage);
+  const newMessage = document.createElement('div');
+  const usernameDiv = document.createElement('div');
+  const messageVal = document.createElement('div');
+  usernameDiv.classList.add('userNickInChat');
+  if (user === userName) {
+    newMessage.classList.add('yourMassage');
+  } else {
+    newMessage.classList.add('odderMessage');
+  }
+
+  usernameDiv.innerText = user;
+  messageVal.classList.add('messageVal');
+  messageVal.innerText = message;
+
+  newMessage.append(usernameDiv, messageVal);
+  messageWindow.append(newMessage);
   messageInput.value = '';
 };
 
-/* const showOnlineUsers = () => {
+const showOnlineUsers = (activeUsers) => {
   usersList.innerHTML = '';
-  onlineUsers.forEach((user) => {
+  activeUsers.map((user) => {
     const newLi = document.createElement('li');
     newLi.innerText = user;
     usersList.append(newLi);
   });
-}; */
+};
 
 const writeNameUsers = () => {
-  yourName = userNameInput.value;
-  divs.forEach((div) => div.classList.toggle('hideDiv'));
-  socket.emit('Login', yourName);
+  if (userName === undefined || userNameInput.value === userName) {
+    userName = userNameInput.value;
+    divs.forEach((div) => div.classList.toggle('hideDiv'));
+    socket.emit('Login', {
+      userName,
+      id: socket.id,
+    });
+  } else {
+    userNameInput.value = userName;
+    alert('You can\'t change nick');
+  }
 };
+
+socket.on('Login', (name) => {
+  const { activeUsersNick } = name;
+  showOnlineUsers(activeUsersNick);
+});
+
+socket.on('disconnectUser', ({ activeUsersNick }) => {
+  showOnlineUsers(activeUsersNick);
+});
 
 socket.on('message', (data) => {
   const { message, user } = data;
@@ -35,21 +65,23 @@ socket.on('message', (data) => {
   creteMessage(message, user);
 });
 
-socket.on('Login', (name) => {
-  const { user } = name;
-  const newLi = document.createElement('li');
-  newLi.innerText = user;
-  usersList.append(newLi);
-});
-
 messageInput.addEventListener('keyup', (e) => {
   if (e.key === 'Enter') {
+    if (messageInput.value === '') {
+      messageInput.setAttribute('placeholder', 'Message can\'t be empty');
+      return;
+    }
+    messageInput.setAttribute('placeholder', 'Write your message');
     socket.emit('message', messageInput.value);
-    creteMessage(messageInput.value, yourName);
+    creteMessage(messageInput.value, userName);
   }
 });
 userNameInput.addEventListener('keyup', (e) => {
   if (e.key === 'Enter') {
+    if (userNameInput.value === '') {
+      userNameInput.setAttribute('placeholder', 'We need Your nick');
+      return;
+    }
     writeNameUsers();
   }
 });
